02e7c93865fb11605185d5f5e2ed9bbb73e24a4d
lucia <lucia.chen189@gmail.com>
2023-07-27 00:49:06 +0200

    removed prints

--- exercises/static/exercises/amazon_warehouse_newmanager/react-components/SpecificAmazonWarehouse.js
+++ exercises/static/exercises/amazon_warehouse_newmanager/react-components/SpecificAmazonWarehouse.js
@@ -9,28 +9,22 @@ function SpecificAmazonWarehouse(props) {
     console.log("TestShowScreen subscribing to ['update'] events");
     
     const getMapDataAndDraw = (data) => {
-      print("[Draw map] Data map: " + str(data.map))
       if (data.map) {
         const pose = data.map.substring(1, data.map.length - 1);
         const content = pose.split(',').map(function(item) {
           return parseFloat(item);
         })
-        print("pose: " + str(pose))
-        print("content: " + str(content))
         draw((content[0] ), (content[1] ), content[2], content[3]);
       }
     }
     const getPathAndDisplay = (data) => {
-      print("[Draw path] Data array: " + str(data.array))
       if(data.array){
         generatePath(JSON.parse(data.array))
       }
     }
 
     const callback = (message) => {
-      print("\n\n\n\nBIRD EYE -------------------------------")
       const data = message.data.update;
-      print("[callback] Data: " + str(data))
       getMapDataAndDraw(data)
       getPathAndDisplay(data)
     };
